e4071a4b37730bd622a127b0cf8cf8b49c0f5385
InoueAkiraa <akiraij@hotmail.com>
2022-11-04 17:38:41 -0400

    Encontro_40
    Realizado a criação das classes Funcionario e Departamento da camada Repositorio. Ficou pendente a finalização das classes Estado e Profissao da camada Repositorio.

---
+++ EstoqueSolucao/Atacado.Repositorio/RH/FuncionarioRepo.cs
@@ -0,0 +1,96 @@
+﻿using Atacado.Repositorio.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Atacado.DB.EF.Database;
+using Atacado.Repositorio.Base;
+using System.Linq.Expressions;
+
+namespace Atacado.Repositorio.RH
+{
+    public class FuncionarioRepo : BaseRepositorioLong<Funcionario>
+    {
+        private ProjetoAcademiaContext contexto;
+
+        public FuncionarioRepo() : base()
+        {
+            this.contexto = new ProjetoAcademiaContext();
+        }
+
+        public override Funcionario Create(Funcionario instancia)
+        {
+            this.contexto.Funcionarios.Add(instancia);
+            this.contexto.SaveChanges();
+            return instancia;
+        }
+
+        public override Funcionario Delete(long chave)
+        {
+            Funcionario del = this.Read(chave);
+            if (del == null)
+            {
+                return null;
+            }
+            else
+            {
+                this.contexto.Remove(del);
+                this.contexto.SaveChanges();
+                return del;
+            }
+        }
+
+        public override Funcionario Delete(Funcionario instancia)
+        {
+            return this.Delete(instancia.FuncionarioId);
+        }
+
+        public override Funcionario Read(long chave)
+        {
+            return this.contexto.Funcionarios.SingleOrDefault(fun => fun.FuncionarioId == chave);
+        }
+
+        public override IQueryable<Funcionario> Read(Expression<Func<Funcionario, bool>> predicate = null)
+        {
+            if (predicate == null)
+            {
+                return this.contexto.Funcionarios.AsQueryable();
+            }
+            else
+            {
+                return this.contexto.Funcionarios.Where(predicate).AsQueryable();
+            }
+        }
+
+        public override List<Funcionario> Read()
+        {
+            return this.contexto.Funcionarios.ToList();
+        }
+
+        public override Funcionario Update(Funcionario instancia)
+        {
+            Funcionario atu = this.Read(instancia.FuncionarioId);
+            if (atu == null)
+            {
+                return null;
+            }
+            else
+            {
+                atu.Matricula = instancia.Matricula;
+                atu.Nome = instancia.Nome;
+                atu.Sobrenome = instancia.Sobrenome;
+                atu.Sexo = instancia.Sexo;
+                atu.DataNascimento = instancia.DataNascimento;
+                atu.Email = instancia.Email;
+                atu.Ctps = instancia.Ctps;
+                atu.CtpsNum = instancia.CtpsNum;
+                atu.CtpsSerie = instancia.CtpsSerie;
+                atu.Ativo = instancia.Ativo;
+                this.contexto.SaveChanges();
+                return atu;
+            }
+        }
+    }
+}
