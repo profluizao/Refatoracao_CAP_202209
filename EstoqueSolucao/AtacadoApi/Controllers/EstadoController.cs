ad048dcf7e86c9a3cedb6a3019a0dc716dfbc4cd
Thiago Nunes da Cruz <thiagonunesc97@gmail.com>
2022-11-07 14:39:44 -0400

    Encontro_41
    Realizada a criação das controllers Estado e Profissão.

---
+++ EstoqueSolucao/AtacadoApi/Controllers/EstadoController.cs
@@ -0,0 +1,83 @@
+﻿using Atacado.Poco.Geral;
+using Atacado.Servico.Geral;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtacadoApi.Controllers
+{
+    [Route("api/Geral/[controller]")]
+    [ApiController]
+    public class EstadoController : ControllerBase
+    {
+        private EstadoServico servico;
+
+        public EstadoController() : base()
+        {
+            this.servico = new EstadoServico();
+        }
+
+        /// <summary>
+        /// Listar todos os registros da tabela Categoria.
+        /// </summary>
+        /// <returns>Uma lista com todos os registros.</returns>
+        [HttpGet]
+        public List<EstadoPoco> GetAll()
+        {
+            return this.servico.Browse();
+        }
+
+        /// <summary>
+        /// Lista o registro da tabela Categoria de acordo com a chave informada
+        /// </summary>
+        /// <param name="chave">chave primária da tabela</param>
+        /// <returns></returns>
+        [HttpGet("{chave:int}")]
+        public EstadoPoco GetById(int chave)
+        {
+            return this.servico.Read(chave);
+        }
+
+        /// <summary>
+        /// Realiza a criação de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco">instância passada como parâmetro</param>
+        /// <returns></returns>
+        [HttpPost]
+        public EstadoPoco Post([FromBody] EstadoPoco poco)
+        {
+            return this.servico.Add(poco);
+        }
+
+        /// <summary>
+        /// Realiza a alteração de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco">instância passada como parâmetro</param>
+        /// <returns></returns
+        [HttpPut]
+        public EstadoPoco Put([FromBody] EstadoPoco poco)
+        {
+            return this.servico.Edit(poco);
+        }
+
+        /// <summary>
+        /// Realiza a exclusão de um registro de acordo com a chave informada
+        /// </summary>
+        /// <param name="chave">chave primária da tabela</param>
+        /// <returns></returns>
+        [HttpDelete("{chave:int}")]
+        public EstadoPoco DeleteById(int chave)
+        {
+            return this.servico.Delete(chave);
+        }
+
+        /// <summary>
+        /// Realiza a exclusão de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco">instância passada como parâmetro</param>
+        /// <returns></returns>
+        [HttpDelete]
+        public EstadoPoco Delete([FromBody] EstadoPoco poco)
+        {
+            return this.servico.Delete(poco);
+        }
+    }
+}
---
+++ EstoqueSolucao/AtacadoApi/Controllers/ProfissaoController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Atacado.Poco.RH;
+using Atacado.Servico.RH;
+
+namespace AtacadoApi.Controllers
+{
+    [Route("api/RH/[controller]")]
+    [ApiController]
+    public class ProfissaoController : ControllerBase
+    {
+        private ProfissaoServico servico;
+
+        public ProfissaoController() : base()
+        {
+            this.servico = new ProfissaoServico();
+        }
+
+        /// <summary>
+        /// Listar todos os registros da tabela Categoria.
+        /// </summary>
+        /// <returns>Uma lista com todos os registros.</returns>
+        [HttpGet]
+        public List<ProfissaoPoco> GetAll()
+        {
+            return this.servico.Browse();
+        }
+
+        /// <summary>
+        /// Lista o registro da tabela Categoria de acordo com a chave informada
+        /// </summary>
+        /// <param name="chave">chave primária da tabela</param>
+        /// <returns></returns>
+        [HttpGet("{chave:int}")]
+        public ProfissaoPoco GetById(int chave)
+        {
+            return this.servico.Read(chave);
+        }
+
+        /// <summary>
+        /// Realiza a criação de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco">instância passada como parâmetro</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ProfissaoPoco Post([FromBody] ProfissaoPoco poco)
+        {
+            return this.servico.Add(poco);
+        }
+
+        /// <summary>
+        /// Realiza a alteração de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco">instância passada como parâmetro</param>
+        /// <returns></returns
+        [HttpPut]
+        public ProfissaoPoco Put([FromBody] ProfissaoPoco poco)
+        {
+            return this.servico.Edit(poco);
+        }
+
+        /// <summary>
+        /// Realiza a exclusão de um registro de acordo com a chave informada
+        /// </summary>
+        /// <param name="chave">chave primária da tabela</param>
+        /// <returns></returns>
+        [HttpDelete("{chave:int}")]
+        public ProfissaoPoco DeleteById(int chave)
+        {
+            return this.servico.Delete(chave);
+        }
+
+        /// <summary>
+        /// Realiza a exclusão de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco">instância passada como parâmetro</param>
+        /// <returns></returns>
+        [HttpDelete]
+        public ProfissaoPoco Delete([FromBody] ProfissaoPoco poco)
+        {
+            return this.servico.Delete(poco);
+        }
+    }
+}
