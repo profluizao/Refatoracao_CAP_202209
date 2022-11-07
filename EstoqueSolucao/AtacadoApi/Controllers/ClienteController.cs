5664f9d7a534cef09c77ed60be3d6bc9fc9eace1
MarlonVictorr <112974081+MarlonVictorr@users.noreply.github.com>
2022-11-07 14:20:50 -0400

    Encontro_41
    Criei as classes Cliente e funcionario no Atacado Api

---
+++ EstoqueSolucao/AtacadoApi/Controllers/ClienteController.cs
@@ -0,0 +1,91 @@
+﻿using Atacado.Poco.Estoque;
+using Atacado.Poco.Geral;
+using Atacado.Servico.Geral;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtacadoApi.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/estoque/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private ClienteServico servico;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ClienteController() : base()
+        {
+            this.servico = new ClienteServico();
+        }
+
+        /// <summary>
+        /// Lista todos os clientes da tabela 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<ClientePoco> GetAll()
+        {
+            return this.servico.Browse();
+        }
+
+        /// <summary>
+        /// Lista o cliente de acordo com o codigo informado 
+        /// </summary>
+        /// <param name="chave"></param>
+        /// <returns></returns>
+        [HttpGet("{chave:int}")]
+        public ClientePoco GetById(int chave)
+        {
+            return this.servico.Read(chave);
+        }
+
+        /// <summary>
+        /// Realiza a criação de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ClientePoco Post([FromBody] ClientePoco poco)
+        {
+            return this.servico.Edit(poco);
+        }
+
+        /// <summary>
+        /// Realiza a alteração de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public ClientePoco Put([FromBody] ClientePoco poco)
+        {
+            return this.servico.Edit(poco);
+        }
+
+        /// <summary>
+        /// Realiza a exclusão de um registro de acordo com a chave primária informada
+        /// </summary>
+        /// <param name="chave"></param>
+        /// <returns></returns>
+        [HttpDelete("{chave:int}")]
+        public ClientePoco DeleteById(int chave)
+        {
+            return this.servico.Delete(chave);
+        }
+
+        /// <summary>
+        /// Realiza a exclusão de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public ClientePoco Delete([FromBody] ClientePoco poco)
+        {
+            return this.servico.Delete(poco);
+        }
+    }
+}
---
+++ EstoqueSolucao/AtacadoApi/Controllers/FuncionarioController.cs
@@ -0,0 +1,90 @@
+﻿using Atacado.Poco.RH;
+using Atacado.Servico.RH;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtacadoApi.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/estoque/[controller]")]
+    [ApiController]
+    public class FuncionarioController : ControllerBase
+    {
+        private FuncionarioServico servico;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public FuncionarioController() : base()
+        {
+            this.servico = new FuncionarioServico();
+        }
+
+        /// <summary>
+        /// Listar todos os registros da tabela Categoria.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<FuncionarioPoco> GetAll()
+        {
+            return this.servico.Browse();
+        }
+
+        /// <summary>
+        /// Lista o registro da tabela Categoria de acordo com a chave primária informada
+        /// </summary>
+        /// <param name="chave"></param>
+        /// <returns></returns>
+        [HttpGet("{chave:int}")]
+        public FuncionarioPoco GetById(int chave)
+        {
+            return this.servico.Read(chave);
+        }
+
+        /// <summary>
+        /// Realiza a criação de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public FuncionarioPoco Post([FromBody] FuncionarioPoco poco)
+        {
+            return this.servico.Add(poco);
+        }
+
+        /// <summary>
+        /// Realiza a alteração de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public FuncionarioPoco Put([FromBody] FuncionarioPoco poco)
+        {
+            return this.servico.Edit(poco);
+        }
+
+        /// <summary>
+        /// Realiza a exclusão de um registro de acordo com a chave primária informada
+        /// </summary>
+        /// <param name="chave"></param>
+        /// <returns></returns>
+        [HttpDelete("{chave:int}")]
+        public FuncionarioPoco DeleteById(int chave)
+        {
+            return this.servico.Delete(chave);
+        }
+
+        /// <summary>
+        /// Realiza a exclusão de um registro através de uma instância
+        /// </summary>
+        /// <param name="poco"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public FuncionarioPoco Delete([FromBody] FuncionarioPoco poco)
+        {
+            return this.servico.Delete(poco);
+        }
+    }
+}
