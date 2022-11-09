1f40baabfa10bf9f2a4c35751876314ffb18ee54
InoueAkiraa <akiraij@hotmail.com>
2022-11-09 14:51:04 -0400

    Encontro_43
    Iniciado a fase de refatoração do AtacadoAPI, onde iremos atualizar a camada do serviço.

---
+++ EstoqueSolucao/Atacado.Repositorio/Base/GenericRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Atacado.DB.EF.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Atacado.Repositorio.Base
+{
+    public class GenericRepository<TDominio> : IGenericRepository<TDominio> where TDominio : class
+    {
+        private ProjetoAcademiaContext context;
+
+        private DbSet<TDominio> table;
+
+        public GenericRepository()
+        {
+            this.context = new ProjetoAcademiaContext();
+            this.table = this.context.Set<TDominio>();
+        }
+
+        public IQueryable<TDominio> Browseable(Expression<Func<TDominio, bool>> predicate = null)
+        {
+            if (predicate == null)
+            {
+                return this.table.AsQueryable();
+            }
+            else
+            {
+                return this.table.Where(predicate);
+            }
+        }
+
+        public IEnumerable<TDominio> GetAll(int? take = null, int? skip = null)
+        {
+            if (skip == null)
+            {
+                return this.table.ToList();
+            }
+            else
+            {
+                return this.table.Skip(skip.Value).Take(take.Value).ToList();
+            }
+        }
+
+        public TDominio GetById(object id)
+        {
+            return this.table.Find(id);
+        }
+
+        public TDominio Insert(TDominio obj)
+        {
+            this.table.Add(obj);
+            this.context.SaveChanges();
+            return obj;
+        }
+
+        public TDominio Update(TDominio obj)
+        {
+            this.table.Attach(obj);
+            this.context.Entry(obj).State = EntityState.Modified;
+            this.context.SaveChanges();
+            return obj;
+        }
+        public TDominio Delete(object id)
+        {
+            TDominio tabelaExistente = this.GetById(id);
+            this.table.Remove(tabelaExistente);
+            this.context.SaveChanges();
+            return tabelaExistente;
+        }
+        public void Save()
+        {
+            this.context.SaveChanges();
+        }
+    }
+}
