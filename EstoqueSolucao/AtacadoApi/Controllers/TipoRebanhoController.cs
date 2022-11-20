f3c32fc6b6f7eadaaf2a8decf6532167ff51e00f
MarlonVictorr <112974081+MarlonVictorr@users.noreply.github.com>
2022-11-20 11:06:47 -0400

    Encontro_47 Update
    Realizado a criação da controller TipoRebanho.
    
    Desenvolvedores presentes: Marlon Victor, Rafael Gemelli e Renato Akira.

---
+++ EstoqueSolucao/AtacadoApi/Controllers/TipoRebanhoController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Atacado.Servico.Pecuaria;
+using Atacado.DB.EF.Database;
+using Atacado.Poco.Pecuaria;
+
+namespace AtacadoApi.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/pecuaria[controller]")]
+    [ApiController]
+    public class TipoRebanhoController : ControllerBase
+    {
+        private TipoRebanhoServico servico;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public TipoRebanhoController() : base()
+        {
+            this.servico = new TipoRebanhoServico();
+        }
+
+        /// <summary>
+        /// Listar todos os registros da tabela.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<List<TipoRebanhoPoco>> GetAll()
+        {
+            try
+            {
+                List<TipoRebanhoPoco> listaPoco = this.servico.Listar();
+                return Ok(listaPoco);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Lista o Tipo Rebanho pelo código
+        /// </summary>
+        /// <param name="chave"></param>
+        /// <returns></returns>
+        [HttpGet("{chave:int}")]
+        public ActionResult<TipoRebanhoPoco> GetById(int chave)
+        {
+            try
+            {
+                TipoRebanhoPoco poco = this.servico.PesquisarPorChave(chave);
+                return Ok(poco);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Cria um novo registro na tabela 
+        /// </summary>
+        /// <param name="poco"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult<TipoRebanhoPoco> Post([FromBody] TipoRebanhoPoco poco)
+        {
+            try
+            {
+                TipoRebanhoPoco nova = this.servico.Inserir(poco);
+                return Ok(nova);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Atualiza os dados da tabela 
+        /// </summary>
+        /// <param name="poco"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult<TipoRebanhoPoco> Put([FromBody] TipoRebanhoPoco poco)
+        {
+            try
+            {
+                TipoRebanhoPoco atPoco = this.servico.Alterar(poco);
+                return Ok(atPoco);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Apaga um registro por codigo informado
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <returns></returns>
+        [HttpDelete("{chave:int}")]
+        public ActionResult<TipoRebanhoPoco> DeleteById(int codigo)
+        {
+            try
+            {
+                TipoRebanhoPoco delPoco = this.servico.Excluir(codigo);
+                return Ok(delPoco);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+    }
+}
