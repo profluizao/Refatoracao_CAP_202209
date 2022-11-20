958fdb65c0879e778bfcdd95db7fef9522182d98
Rafael Martins <rafaelgemelli1@hotmail.com>
2022-11-20 10:12:36 -0400

    Encontro_47 Update
    Foi realizado a criação da classe TipoRebanho no Entity Framework.
    
    Integrantes presentes: Marlon Victor, Rafael Gemelli e Renato Akira

---
+++ EstoqueSolucao/Atacado.DB.EF/Database/TipoRebanho.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atacado.DB.EF.Database
+{
+    [Table("Tipo_Rebanho", Schema = "dbo")]
+    public partial class TipoRebanho
+    {
+        [Key]
+        [Column(name: "ID_Tipo")]
+        public int CodigoTipo { get; set; }
+
+        [Column(name: "Descricao")]
+        [Unicode(false)]
+        public string Descricao { get; set; } = null!;
+
+        [Column(name: "Situacao")]
+        public bool? Situacao { get; set; }
+
+        [Column(name: "DataInclusao", TypeName = "datetime")]
+        public DateTime? DataInclusao { get; set; }
+
+        [Column(name: "DataAlteracao", TypeName = "datetime")]
+        public DateTime? DataAlteracao { get; set; }
+
+        [Column(name: "DataExclusao", TypeName = "datetime")]
+        public DateTime? DataExclusao { get; set; }
+    }
+}
