3934a0776e40e9b7b0bf29dccc8e3e702213a14d
Rafael Martins <rafaelgemelli1@hotmail.com>
2022-11-20 10:36:26 -0400

    Encontro_47 Update
    Foi realizado a criação das classes RebanhoServico e TipoRebanhoServico no Atacado.Servico.
    
    Desenvolvedores presentes: Marlon Victor, Renato Akira e Rafael Gemelli

---
+++ EstoqueSolucao/Atacado.Servico/Pecuaria/RebanhoServico.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Atacado.Servico.Base;
+using Atacado.DB.EF.Database;
+using Atacado.Poco.Pecuaria;
+using System.Linq.Expressions;
+using Atacado.Poco.Estoque;
+namespace Atacado.Servico.Pecuaria
+{
+    public class RebanhoServico : GenericService<Rebanho, RebanhoPoco>
+    {
+        public override List<RebanhoPoco> Consultar(Expression<Func<Rebanho, bool>>? predicate = null)
+        {
+            IQueryable<Rebanho> query;
+            if (predicate == null)
+            {
+                query = this.genrepo.Browseable(null);
+            }
+            else
+            {
+                query = this.genrepo.Browseable(predicate);
+            }
+            return this.ConverterPara(query);
+        }
+
+        public override List<RebanhoPoco> Listar(int? take = null, int? skip = null)
+        {
+            IQueryable<Rebanho> query;
+            if (skip == null)
+            {
+                query = this.genrepo.GetAll();
+            }
+            else
+            {
+                query = this.genrepo.GetAll(take, skip);
+            }
+            return ConverterPara(query);
+        }
+
+        public override List<RebanhoPoco> ConverterPara(IQueryable<Rebanho> query)
+        {
+            return query.Select(reb =>
+                    new RebanhoPoco()
+                    {
+                        CodigoRebanho = reb.CodigoRebanho,
+                        AnoReferencia = reb.AnoReferencia,
+                        CodigoMunicipio = reb.CodigoMunicipio,
+                        CodigoTipoRebanho = reb.CodigoTipoRebanho,
+                        TipoRebanho = reb.TipoRebanho,
+                        Quantidade = reb.Quantidade,
+                        Situacao = reb.Situacao,
+                        DataInclusao = reb.DataInclusao,
+                        DataAlteracao = reb.DataAlteracao,
+                        DataExclusao = reb.DataExclusao
+                    }
+            )
+            .ToList();
+        }
+    }
+}
---
+++ EstoqueSolucao/Atacado.Servico/Pecuaria/TipoRebanhoServico.cs
@@ -0,0 +1,41 @@
+﻿using Atacado.DB.EF.Database;
+using Atacado.Poco.Pecuaria;
+using Atacado.Servico.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atacado.Servico.Pecuaria
+{
+    public class TipoRebanhoServico : GenericService<TipoRebanho, TipoRebanhoPoco> 
+    {
+        public override List<TipoRebanhoPoco> Consultar(Expression<Func<TipoRebanho, bool>>? predicate = null)
+        {
+            IQueryable<TipoRebanho> query;
+            if (predicate == null)
+            {
+                query = this.genrepo.Browseable(null);
+            }
+            else
+            {
+                query = this.genrepo.Browseable(predicate);
+            }
+            List<TipoRebanhoPoco> listaPoco = query.Select(tip =>
+                    new TipoRebanhoPoco()
+                    {
+                        CodigoTipo = tip.CodigoTipo,
+                        Descricao = tip.Descricao,
+                        Situacao = tip.Situacao,
+                        DataInclusao = tip.DataInclusao,
+                        DataAlteracao = tip.DataAlteracao,
+                        DataExclusao = tip.DataExclusao
+                    }
+            )
+            .ToList();
+            return listaPoco;
+        }
+    }
+}
