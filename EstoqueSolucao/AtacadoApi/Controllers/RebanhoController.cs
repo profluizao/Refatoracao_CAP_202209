ae259454d7f4a9df3dd303a518ee09a07cc5dfe4
InoueAkiraa <akiraij@hotmail.com>
2022-11-20 10:38:00 -0400

    Encontro_47 Update
    Realizado criação da Controller "Rebanho". Que irá realizar os métodos CRUDs, focando na performance das buscas.
    
    Desenvolvedores presentes: Marlon Victor, Rafael Gemelli e Renato Akira.

---
+++ EstoqueSolucao/AtacadoApi/Controllers/RebanhoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtacadoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RebanhoController : ControllerBase
+    {
+    }
+}
