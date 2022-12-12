22b851c49bbf5ed13c00dfdd13d7170bbde8d2ed
MarlonVictorr <112974081+MarlonVictorr@users.noreply.github.com>
2022-12-12 17:29:18 -0400

    Encontro_64
    Criação das classes de serviço cidade, evento, região e controllers região e rota

--- ProjetoViajeFacil/CSharp/ProjetoViajeFacil/ViajeFacil.Service/Viagem/CidadeService.cs
+++ ProjetoViajeFacil/CSharp/ProjetoViajeFacil/ViajeFacil.Service/Viagem/CidadeService.cs
@@ -7,7 +7,80 @@ using ViajeFacil.Service.Base;
 
 namespace ViajeFacil.Service.Viagem
 {
-    public class CidadeService 
+    public class CidadeService : GenericService <Cidade,CidadePoco>
     {
+        public CidadeService(ViajeFacilContexto contexto) : base(contexto)
+        { }
+
+        public override List<CidadePoco> Listar(int? take = null, int? skip = null)
+        {
+            IQueryable<Cidade> query;
+            if(skip == null)
+            {
+                query = this.genrepo.GetAll();
+            }
+            else
+            {
+                query = this.genrepo.GetAll(take,skip);
+            }
+            return this.ConverterPara(query);
+        }
+
+        public override List<CidadePoco> Consultar(System.Linq.Expressions.Expression<Func<Cidade, bool>>? predicate = null)
+        {
+            IQueryable<Cidade> query;
+            if(predicate == null)
+            {
+                query = this.genrepo.Browseable(null);
+            }
+            else
+            {
+                query = this.genrepo.Browseable(predicate);
+            }
+            return this.ConverterPara(query);
+        }
+
+        public override List<CidadePoco> Vasculhar(int? take = null, int? skip = null, System.Linq.Expressions.Expression<Func<Cidade, bool>>? predicate = null)
+        {
+            IQueryable<Cidade> query;
+            if (skip == null)
+            {
+                if (predicate == null)
+                {
+                    query = this.genrepo.Browseable(null);
+                }
+                else
+                {
+                    query = this.genrepo.Browseable(predicate);
+                }
+            }
+            else
+            {
+                if (predicate == null)
+                {
+                    query = this.genrepo.GetAll(take, skip);
+                }
+                else
+                {
+                    query = this.genrepo.Searchable(take, skip, predicate);
+                }
+            }
+            return this.ConverterPara(query);
+        }
+
+        public override List<CidadePoco> ConverterPara(IQueryable<Cidade> query)
+        {
+            return query.Select(cid =>
+            new CidadePoco()
+            {
+                CodigoCidade = cid.CodigoCidade,
+                Nome = cid.Nome,
+                CodigoIbge7 = cid.CodigoIBGE7,
+                SiglaUf = cid.SiglaUF,
+                CodigoEstado = cid.CodigoEstado
+            }
+            )
+                .ToList();
+        }
     }
 }
---
+++ ProjetoViajeFacil/CSharp/ProjetoViajeFacil/ViajeFacil.Service/Viagem/RegiaoService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ViajeFacil.Service.Base;
+
+namespace ViajeFacil.Service.Viagem
+{
+    public class RegiaoService : GenericService<Regiao,RegiaoPoco>
+    {
+        public RegiaoService(ViajeFacilContexto contexto) : base(contexto)
+        { }
+
+        public override List<RegiaoPoco> Listar(int? take, int? skip = null)
+        {
+            IQueryable<Regiao> query;
+            if (skip == null)
+            {
+                query = this.genrepo.GetAll();
+            }
+            else
+            {
+                query = this.genrepo.GetAll(take, skip);
+            }
+            return this.ConverterPara(query);
+        }
+
+        public override List<RegiaoPoco> Consultar(Expression<Func<Regiao, bool>>? predicate = null)
+        {
+            IQueryable<Regiao> query;
+            if (predicate == null)
+            {
+                query = this.genrepo.Browseable(null);
+            }
+            else
+            {
+                query = this.genrepo.Browseable(predicate);
+            }
+            return this.ConverterPara(query);
+        }
+
+        public override List<RegiaoPoco> ConverterPara(IQueryable<Regiao> query)
+        {
+            return query.Select(reg =>
+            new RegiaoPoco()
+            {
+                CodigoRegiao = reg.CodigoRegiao,
+                Nome = reg.Nome,
+                CodigoPais = reg.CodigoPais
+            }
+            )
+                .ToList();
+        }
+    }
+}
